6008d43a38d0d67c5f09a40cdf10aaa8e20fc364
Smolllll1 <Smolllll192@gmail.com>
2023-01-14 12:37:56 +0200

    fix HW1_Pro

--- HW1_Pro.py
+++ HW1_Pro.py
@@ -20,22 +20,18 @@ class Shopper:
     def __str__(self):
         return f'{self.surname}.{self.name[0]}   tel: {self.phone}'
 class Order:
-    def __init__(self, shoper, product):
+    def __init__(self, shoper):
         self.shoper = shoper
-        self.product = product
-    def total_price(self, prace):
-        self.prace = prace.price
-        self.tovar = prace.product
         self.suma = {}
+    def total_price(self, goods):
+        self.prace = goods.price
+        self.tovar = goods.product
         if self.tovar in self.suma:
             self.suma[self.tovar] = self.suma.get(self.tovar) + int(self.prace)
         else:
             self.suma[self.tovar] = int(self.prace)
-
-
     def __str__(self):
         return  f'\n{"*"*40}\n{self.shoper}\nThe total cost of the order: {sum(self.suma.values())} $\n{"*"*40}'
-
 
 x1 = Goods('120', 'chemistry', '20*30')
 x2 = Goods('100', 'cooking', '50*60')
@@ -45,7 +41,7 @@ shoper_1 = Shopper('Ivan', 'Ivanov', '099-999-99-99')
 shoper_2 = Shopper('Ivan1', 'Ivanov1', '088-888-88-88')
 shoper_3 = Shopper('Ivan2', 'Ivanov2', '077-777-77-77')
 
-order = Order(shoper_1, x1)
+order = Order(shoper_1)
 order.total_price(x1)
 order.total_price(x2)
 order.total_price(x1)
