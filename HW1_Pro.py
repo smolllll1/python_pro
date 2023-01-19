2995d8598450d96e5d19dbe572506c093178b456
Smolllll1 <Smolllll192@gmail.com>
2023-01-19 13:17:21 +0200

    add HW3_Pro(modify HW1 and HW2)

--- HW1_Pro.py
+++ HW1_Pro.py
@@ -5,45 +5,95 @@
 # 3. Створіть клас "Замовлення". Замовлення може містити декілька товарів певної кількості.
 # Замовлення має містити дані про користувача, який його здійснив. Реалізуйте метод обчислення сумарної вартості замовлення.
 # Визначте метод str() для коректного виведення інформації про це замовлення.
-class Goods:
-    def __init__(self, price, product, dimensions):
+
+# HW3 . Модифікуйте Перше домашнє завдання так, щоб при спробі встановити від'ємну або нульову вартість товару викликалася виняткова ситуація
+# (тип виняткової ситуації треба самостійно реалізувати).
+
+class Incorrect_number(Exception):
+    def __init__(self, msg, correct):
+        self.msg = msg
+        self.correct = correct
+
+    def __str__(self):
+        return f'{self.msg}, {self.correct}'
+
+class Product:
+
+    def __init__(self, title: str, price: int | float):
+
+        self.title = title
         self.price = price
-        self.product = product
-        self.dimensions = dimensions
+
     def __str__(self):
-        return f'{self.product} - {self.price}$   {self.dimensions}'
-class Shopper:
-    def __init__(self, name, surname, phone):
-        self.name = name
+        return f'{self.title}: {self.price} UAH'
+
+
+class Customer:
+
+    def __init__(self, surname, name, phone):
         self.surname = surname
+        self.name = name
         self.phone = phone
+
     def __str__(self):
-        return f'{self.surname}.{self.name[0]}   tel: {self.phone}'
-class Order:
-    def __init__(self, shoper):
-        self.shoper = shoper
-        self.suma = {}
-    def total_price(self, goods):
-        self.prace = goods.price
-        self.tovar = goods.product
-        if self.tovar in self.suma:
-            self.suma[self.tovar] = self.suma.get(self.tovar) + int(self.prace)
-        else:
-            self.suma[self.tovar] = int(self.prace)
+        return f'\n{self.surname} {self.name[0]}. tel:  {self.phone}\n{"*"*35}'
+
+
+class Cart:
+
+    MAX_LIMIT = 40
+
+    def __init__(self, customer: Customer):
+        self.customer = customer
+        self.__products = []
+        self.__quantities = []
+
+    def get_weight(self):
+        return sum(self.__quantities)
+
+    def add_product(self, product: Product, quantity: int | float = 1):
+        if self.get_weight() + quantity <= Cart.MAX_LIMIT:
+            if product.price <= 0:
+                raise Incorrect_number(f'{product.title}!!!   Incorrect prise', 'mast be > 0')
+            self.__products.append(product)
+            self.__quantities.append(quantity)
+
+    def total(self):
+        result = 0
+        for product, quantity in zip(self.__products, self.__quantities):
+            result += product.price * quantity
+
+        return result
+
     def __str__(self):
-        return  f'\n{"*"*40}\n{self.shoper}\nThe total cost of the order: {sum(self.suma.values())} $\n{"*"*40}'
+        result = f'{self.customer}\n'
+        result += '\n'.join(map(lambda item: f'{item[0]} x {item[1]} = {item[0].price * item[1]} UAH',
+                      zip(self.__products, self.__quantities)))
+        result += f'\nTotal: {self.total()} UAH'
+        return result
 
-x1 = Goods('120', 'chemistry', '20*30')
-x2 = Goods('100', 'cooking', '50*60')
-x3 = Goods('600', 'confectioner', '80*90')
 
-shoper_1 = Shopper('Ivan', 'Ivanov', '099-999-99-99')
-shoper_2 = Shopper('Ivan1', 'Ivanov1', '088-888-88-88')
-shoper_3 = Shopper('Ivan2', 'Ivanov2', '077-777-77-77')
+x_1 = Product('banana', 1)
+x_2 = Product('apple', 25)
+x_3 = Product('orange', -35)
 
-order = Order(shoper_1)
-order.total_price(x1)
-order.total_price(x2)
-order.total_price(x1)
-order.total_price(x3)
-print(order)
+customer_1 = Customer('Ivanov', 'Ivan', '123456789')
+customer_2 = Customer('Ivanov', 'Petro', '123456799')
+
+order_1 = Cart(customer_1)
+try:
+    order_1.add_product(x_1)
+    order_1.add_product(x_2, 2)
+    order_1.add_product(x_3, 35)
+except Incorrect_number as error:
+    print(error)
+
+print(order_1)
+
+
+order_2 = Cart(customer_2)
+try:
+    order_2.add_product(x_2, 10)
+except Incorrect_number as error:
+    print(error)
+print(order_2)
--- HW2_Pro.py
+++ HW2_Pro.py
@@ -4,17 +4,29 @@
 # Реалізуйте методи додавання, видалення студента та метод пошуку студента за прізвищем.
 # Визначте для Групи метод str() для повернення списку студентів у вигляді рядка.
 
+# HW3 2. Модифікуйте Друге домашнє завдання так, щоб при спробі додавання до групи більше 10-ти студентів,
+# викликалася виняткова ситуація (тип виняткової ситуації треба самостійно реалізувати).
+
+import logging
+class Incorrect_number(Exception):
+    def __init__(self, msg, correct):
+        self.msg = msg
+        self.correct = correct
+
+    def __str__(self):
+        return f'{self.msg}, {self.correct}'
+
 class Man:
-    def __init__(self, name, surname):
+    def __init__(self, surname, name):
         self.name = name
         self.surname = surname
 
     def __str__(self):
-        return f'{self.name}. {self.surname[0]}'
+        return f'{self.surname}. {self.name[0]}'
 
 class Student(Man):
-    def __init__(self, name, surname, phon):
-        super().__init__(name, surname)
+    def __init__(self, surname, name, phon):
+        super().__init__(surname, name)
         self.phon = phon
 
     def listed(self):
@@ -25,33 +37,44 @@ class Student(Man):
         return f'{super().__str__()} - {self.phon}'
 
 class Group:
-    def __init__(self, castomer: Student):
-        self.castomer = castomer
+    def __init__(self, title, max_limit=10):
+        self.title = title
+        self.max_limit = max_limit
         self.__students = []
-        self.limit = 1
-        self.input_name = input('enter a name: ')
-        self.output = ''
+
 
     def add_student(self, arg: Student):
-        if Student.listed(arg) not in self.__students and self.limit <= 10:
-            self.__students.append(Student.listed(arg))
-            self.limit += 1
+        if len(self.__students) >= self.max_limit:
+            raise Incorrect_number('limit exceeded!!!', f'  maximum value: {self.max_limit}')
+        for item in self.__students:
+            if (item.surname, item.name, item.phon) == (arg.surname, arg.name, arg.phon):
+                return None
+        self.__students.append(arg)
+        logger.info(f'appending student! {arg}')
 
     def remove_student(self, arg: Student):
-        self.__students.remove(Student.listed(arg))
+        if arg in self.__students:
+            self.__students.remove(arg)
+            logger.info(f'removing student! {arg}')
 
-    def search(self):
-        for i in self.__students:
-            for j in i:
-                if self.input_name == j:
-                    return f'{" ".join(i)}'
+    def search(self, surname: str):
+        res = []
+        for item in self.__students:
+            if item.surname == surname:
+                res.append(item)
+        return res
 
     def __str__(self):
-        for i in self.__students:
-            self.output += " ".join(i[:-1]) + ', '
-        return self.output[:-2]
+        return f'{self.title}\nStudents:\n' + '\n'.join(map(str, self.__students))
 
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s %(levelname)-8s %(message)s')
 
+filehandler = logging.FileHandler('logger.log')
+filehandler.setLevel(logging.INFO)
+filehandler.setFormatter(formatter)
+logger.addHandler(filehandler)
 
 
 m1 = Student('Ivanov', 'Ivan', '11111111')
@@ -64,21 +87,32 @@ m7 = Student('Mashanov', 'Masha', '77777777')
 m8 = Student('Olyanov', 'Olya', '88888888')
 m9 = Student('Iranov', 'Ira', '9999999999')
 m10 = Student('Vityanov', 'Vitya', '1000000000')
+m11 = Student('Viktorov', 'Viktor', '21212121212')
 
-s = Group(m1)
+gpuop_1 = Group('Gruop 1')
+gpuop_2 = Group('Gruop 2')
+try:
+    gpuop_1.add_student(m1)
+    gpuop_2.add_student(m2)
+    gpuop_1.add_student(m3)
+    gpuop_2.add_student(m4)
+    gpuop_1.add_student(m5)
+    gpuop_2.add_student(m6)
+    gpuop_1.add_student(m7)
+    gpuop_1.add_student(m8)
+    gpuop_2.add_student(m9)
+    gpuop_2.add_student(m10)
+except Incorrect_number as error:
+    print(error)
+finally:
+    gpuop_2.remove_student(m2)
 
-s.add_student(m1)
-s.add_student(m2)
-s.add_student(m3)
-s.add_student(m4)
-s.add_student(m5)
-s.add_student(m6)
-s.add_student(m7)
-s.add_student(m8)
-s.add_student(m9)
-s.add_student(m10)
 
-s.remove_student(m2)
 
-print(s.search())
-print(s)
+print(gpuop_2)
+print(gpuop_1)
+
+search_name = 'Petrov'
+res = gpuop_1.search(search_name)
+print('\n', '*'*40, '\n')
+print('*'.join(map(str, res)))
